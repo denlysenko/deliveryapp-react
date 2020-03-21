1188791913e825a8bf2f2738678355fe78b35a0e
denlysenko <den.lysenko.mail@gmail.com>
2020-03-21 21:28:27 +0200

    Add CreateOrder layout

--- libs/pages/create-order/src/lib/CreateOrder.tsx
+++ libs/pages/create-order/src/lib/CreateOrder.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Steps } from 'primereact/steps';
+import { MenuItem } from 'primereact/api';
 
 import { StyledCreateOrder } from './StyledCreateOrder';
 
 export const CreateOrder = () => {
+  const [items] = useState<MenuItem[]>([
+    { label: 'Destination' },
+    { label: 'Cargo' },
+    { label: 'Sender' }
+  ]);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSelect = (event: {
+    originalEvent: Event;
+    item: MenuItem;
+    index: number;
+  }) => {
+    setActiveIndex(event.index);
+  };
+
   return (
     <StyledCreateOrder>
-      <h1>Welcome to create-order page component!</h1>
+      <div className="p-grid">
+        <div className="p-col-12">
+          <div className="card">
+            <Steps
+              model={items}
+              activeIndex={activeIndex}
+              onSelect={handleSelect}
+              readOnly={false}
+            />
+            <div className="ui-widget-content">
+              {activeIndex === 0 && <div>Destination form goes here</div>}
+              {activeIndex === 1 && <div>Cargo form goes here</div>}
+              {activeIndex === 2 && <div>Sender form goes here</div>}
+            </div>
+          </div>
+        </div>
+      </div>
     </StyledCreateOrder>
   );
 };
--- libs/pages/create-order/src/lib/StyledCreateOrder.tsx
+++ libs/pages/create-order/src/lib/StyledCreateOrder.tsx
@@ -1,5 +1,40 @@
 import styled from 'styled-components';
 
 export const StyledCreateOrder = styled.div`
-  color: pink;
+  .p-steps-item {
+    width: 33.333%;
+
+    .p-menuitem-link {
+      .p-steps-number {
+        @media (max-width: 640px) {
+          margin-bottom: 2.5em !important;
+        }
+      }
+
+      .p-steps-title {
+        @media (max-width: 640px) {
+          display: none !important;
+        }
+      }
+    }
+  }
+
+  .card {
+    margin-bottom: 0;
+    height: calc(100vh - 99px);
+  }
+
+  .ui-widget-content {
+    padding: 15px 0;
+    width: auto;
+
+    .ui-inputtext {
+      font-weight: 300;
+    }
+
+    @media (min-width: 1100px) {
+      width: 960px;
+      margin: 0 auto;
+    }
+  }
 `;
