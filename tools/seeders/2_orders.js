fabc9a09f0bc77243ae583625a82c53365855152
denlysenko <den.lysenko.mail@gmail.com>
2020-05-15 22:46:20 +0300

    generate test orders

--- tools/seeders/2_orders.js
+++ tools/seeders/2_orders.js
@@ -1,45 +1,69 @@
 'use strict';
 
+const cities = [
+  'Kyiv',
+  'Minsk',
+  'London',
+  'Prague',
+  'Madrid',
+  'Rome',
+  'Berlin',
+  'Paris'
+];
+const cargos = [
+  'Computers',
+  'Sofa',
+  'TV',
+  'Phone',
+  'Chair',
+  'Piano',
+  'Oven',
+  'Monitor',
+  'Books'
+];
+
+function generateOrders() {
+  const orders = [];
+
+  for (let i = 0; i < 13; i++) {
+    const cityFrom = cities[Math.floor(Math.random() * cities.length)];
+    const cityTo = cities[Math.floor(Math.random() * cities.length)];
+    const cargoName = cargos[Math.floor(Math.random() * cargos.length)];
+
+    const order = {
+      id: i + 1,
+      cityFrom,
+      cityTo,
+      addressFrom: `From ${cityFrom}, 12`,
+      addressTo: `To ${cityTo}, 1`,
+      cargoName,
+      additionalData: '',
+      comment: '',
+      cargoWeight: 12,
+      senderName: '',
+      senderCompany: '',
+      senderEmail: 'cy.sender@email.com',
+      senderPhone: '(123) 121-1212',
+      status: 0,
+      paid: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      creatorId: 1,
+      clientId: 1
+    };
+
+    orders.push(order);
+  }
+
+  return orders;
+}
+
 module.exports = {
-  up: function(queryInterface) {
-    return queryInterface.bulkInsert(
-      'orders',
-      [
-        {
-          id: 1,
-          cityFrom: 'Kyiv',
-          cityTo: 'Dnipro',
-          addressFrom: 'From, 12',
-          addressTo: 'To, 1',
-          cargoName: 'Computers',
-          additionalData: '',
-          comment: '',
-          cargoWeight: 12,
-          senderName: '',
-          senderCompany: '',
-          senderEmail: 'cy.sender@email.com',
-          senderPhone: '(123) 121-1212',
-          status: 0,
-          paid: false,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          creatorId: 1,
-          clientId: 1
-        }
-      ],
-      {}
-    );
+  up: function (queryInterface) {
+    return queryInterface.bulkInsert('orders', generateOrders(), {});
   },
 
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.bulkDelete(
-      'orders',
-      {
-        senderEmail: {
-          [Sequelize.Op.in]: ['cy.sender@email.com']
-        }
-      },
-      {}
-    );
+  down: function (queryInterface) {
+    return queryInterface.bulkDelete('orders', {}, {});
   }
 };
