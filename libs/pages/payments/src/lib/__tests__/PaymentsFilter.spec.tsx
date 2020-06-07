fbec280c7d5f8089a560f600ed262145b17e036c
denlysenko <den.lysenko.mail@gmail.com>
2020-06-07 19:42:58 +0300

    add PaymentsFilter tests

--- libs/pages/payments/src/lib/__tests__/PaymentsFilter.spec.tsx
+++ libs/pages/payments/src/lib/__tests__/PaymentsFilter.spec.tsx
@@ -1,11 +1,64 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { PaymentsFilter } from '../PaymentsFilter/PaymentsFilter';
 
+jest.mock('lodash', () => ({
+  ...require.requireActual('lodash'),
+  debounce: jest.fn((fn) => fn)
+}));
+
+const handleFilterChange = jest.fn();
+
 describe('PaymentsFilter', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should render successfully', () => {
-    const { baseElement } = render(<PaymentsFilter />);
-    expect(baseElement).toBeTruthy();
+    const { baseElement } = render(
+      <PaymentsFilter handleFilterChange={handleFilterChange} />
+    );
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  it('should render empty string for searchTerm if filter prop is not present', () => {
+    render(<PaymentsFilter handleFilterChange={handleFilterChange} />);
+    expect(screen.getByPlaceholderText(/search by number/i)).toHaveValue('');
+  });
+
+  it('should render empty string for searchTerm if filter prop is empty', () => {
+    render(
+      <PaymentsFilter filter={{}} handleFilterChange={handleFilterChange} />
+    );
+    expect(screen.getByPlaceholderText(/search by number/i)).toHaveValue('');
+  });
+
+  it('should render searchTerm from filter', () => {
+    render(
+      <PaymentsFilter
+        filter={{ id: 1 }}
+        handleFilterChange={handleFilterChange}
+      />
+    );
+    expect(screen.getByPlaceholderText(/search by number/i)).toHaveValue('1');
+  });
+
+  describe('handleFilterChange', () => {
+    it('should call if searchTerm has changed', () => {
+      render(
+        <PaymentsFilter filter={{}} handleFilterChange={handleFilterChange} />
+      );
+
+      fireEvent.change(screen.getByPlaceholderText(/search by number/i), {
+        target: {
+          value: '2'
+        }
+      });
+
+      expect(handleFilterChange).toBeCalledTimes(1);
+      expect(handleFilterChange).toBeCalledWith({ id: 2 });
+    });
   });
 });
