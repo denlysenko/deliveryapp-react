ae46631f3877c52ec6990b08e937200d8fc0367e
denlysenko <den.lysenko.mail@gmail.com>
2020-06-23 14:54:34 +0300

    add BankDetailsForm tests

--- libs/pages/settings/src/lib/__tests__/BankDetailsForm.spec.tsx
+++ libs/pages/settings/src/lib/__tests__/BankDetailsForm.spec.tsx
@@ -1,11 +1,84 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+
+import { settingsClient } from '@deliveryapp/data-access';
+import { bankDetails } from '@deliveryapp/testing';
 
 import { BankDetailsForm } from '../BankDetailsForm/BankDetailsForm';
 
 describe('BankDetailsForm', () => {
+  beforeEach(() => {
+    jest
+      .spyOn(settingsClient, 'createBankDetails')
+      .mockImplementation(() =>
+        Promise.resolve({ data: { id: bankDetails.id } })
+      );
+
+    jest
+      .spyOn(settingsClient, 'updateBankDetails')
+      .mockImplementation(() =>
+        Promise.resolve({ data: { id: bankDetails.id } })
+      );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should render successfully', () => {
     const { baseElement } = render(<BankDetailsForm />);
     expect(baseElement).toBeTruthy();
+  });
+
+  describe('Submitting', () => {
+    it('should create successfully', async () => {
+      const name = 'Bank Name';
+
+      render(<BankDetailsForm />);
+
+      fireEvent.change(screen.getByTestId('name'), {
+        target: {
+          value: name
+        }
+      });
+
+      fireEvent.click(screen.getByTestId('save'));
+
+      await waitFor(() => {
+        expect(settingsClient.createBankDetails).toBeCalledTimes(1);
+      });
+
+      expect(settingsClient.createBankDetails).toBeCalledWith({
+        name,
+        accountNumber: '',
+        bin: '',
+        swift: ''
+      });
+    });
+
+    it('should update successfully', async () => {
+      const updatedName = 'Updated Name';
+
+      render(<BankDetailsForm bankDetails={bankDetails} />);
+
+      fireEvent.change(screen.getByTestId('name'), {
+        target: {
+          value: updatedName
+        }
+      });
+
+      fireEvent.click(screen.getByTestId('save'));
+
+      await waitFor(() => {
+        expect(settingsClient.updateBankDetails).toBeCalledTimes(1);
+      });
+
+      expect(settingsClient.updateBankDetails).toBeCalledWith(bankDetails.id, {
+        name: updatedName,
+        accountNumber: bankDetails.accountNumber,
+        bin: bankDetails.bin,
+        swift: bankDetails.swift
+      });
+    });
   });
 });
