4c2844b1768f19b7fed8dc4412c7b9eebb2d9a96
denlysenko <den.lysenko.mail@gmail.com>
2020-06-23 14:59:12 +0300

    add Settings tests

--- libs/pages/settings/src/lib/__tests__/Settings.spec.tsx
+++ libs/pages/settings/src/lib/__tests__/Settings.spec.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+
+import { settingsClient } from '@deliveryapp/data-access';
+import { address, bankDetails } from '@deliveryapp/testing';
 
 import { Settings } from '../Settings';
 
 describe('Settings', () => {
-  it('should render successfully', () => {
+  beforeEach(() => {
+    jest
+      .spyOn(settingsClient, 'getAddress')
+      .mockResolvedValue({ data: address });
+    jest
+      .spyOn(settingsClient, 'getBankDetails')
+      .mockResolvedValue({ data: bankDetails });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render successfully', async () => {
     const { baseElement } = render(<Settings />);
+
+    await waitFor(() => {
+      expect(settingsClient.getAddress).toBeCalledTimes(1);
+      expect(settingsClient.getBankDetails).toBeCalledTimes(1);
+    });
+
     expect(baseElement).toBeTruthy();
   });
 });
